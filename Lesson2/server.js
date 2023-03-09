80c81d5b291a6135e171343703e8c5187af9edb7
tuva9 <tuva9@fpt.edu.vn>
2023-03-09 13:52:07 +0700

    Buổi 2

--- Lesson2/server.js
+++ Lesson2/server.js
@@ -1,11 +1,10 @@
+// FRAMEWORK
 const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const bodyParser = require('body-parser')
+const fs = require("fs")
+const path = require("path")
+const bodyParser = require("body-parser")
 const app = express()
-const port = 8000
-
-// app.use(express.json())
+const port = 3000
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -13,33 +12,45 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-const products = [
-    { id: 1, name: "iphone14", price: 1500 },
-    { id: 2, name: "iphone13", price: 1000 }
+const data = [
+    {id: 1, name: "Chuột", price: 100},
+    {id: 2, name: "Bàn phím", price: 200},
+    {id: 3, name: "Màn hình", price: 500},
 ]
 
+// Routings
 app.get('/', (req, res) => {
-    const html = fs.readFileSync(path.join(__dirname, "home.html"), "utf8")
+    // __dirname, __filename
+    // require
+    const html = fs.readFileSync(path.join(__dirname, "home.html"), "utf-8")
     res.send(html)
 })
-
-app.get('/api/products', (req, res) => {
-    res.send(products)
+// Routings
+app.get('/products', (req, res) => {
+    res.send(data)
 })
 
-app.get('/api/products/:id', (req, res) => {
+app.post('/products', (req, res) => {
+    // console.log(req, "create product");
+    const newData = req.body
+    data.push(newData)
+    res.send(data)
+})
+// Routings
+// Get product by ID
+app.get('/products/:id', (req, res) => {
     const id = req.params.id
-    const product = products.find((item) => item.id == id)
+    const product = data.find((item) => item.id == id)
     if (product) {
         res.send(product)
     } else {
-        res.status(400).send("San phan khong ton tai")
+        res.status(400).send("San pham khong ton tai!")
     }
 })
 
-app.post('/api/products', (req, res) => {
-    const body = req.body
-    res.send(body)
+app.get('/product/add', (req, res) => {
+    const html = fs.readFileSync(path.join(__dirname, "product-add.html"), "utf-8")
+    res.send(html)
 })
 
 app.listen(port, () => {
