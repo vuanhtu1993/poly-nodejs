0b7b3813db69ccb28570ffc880b3ce61608a8cdc
tuva9 <tuva9@fpt.edu.vn>
2023-03-11 13:09:45 +0700

    Buổi 3: Update/ Delete

--- Lesson2/server.js
+++ Lesson2/server.js
@@ -3,9 +3,11 @@ const express = require('express')
 const fs = require("fs")
 const path = require("path")
 const bodyParser = require("body-parser")
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+// Middleware
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -18,6 +20,9 @@ const data = [
     {id: 3, name: "Màn hình", price: 500},
 ]
 
+// Logger
+app.use(morgan('combined'))
+
 // Routings
 app.get('/', (req, res) => {
     // __dirname, __filename
@@ -29,12 +34,40 @@ app.get('/', (req, res) => {
 app.get('/products', (req, res) => {
     res.send(data)
 })
-
+// Created
 app.post('/products', (req, res) => {
     // console.log(req, "create product");
     const newData = req.body
     data.push(newData)
     res.send(data)
+})
+
+// Updated
+app.put('/products/:id', (req, res) => {
+    const id = req.params.id
+    const updateData = req.body
+    const productIndex = data.findIndex(item => item.id == id)
+    console.log(productIndex);
+    if (productIndex >= 0) {
+        data[productIndex] = {...data[productIndex], ...updateData}
+        res.send(data[productIndex])
+        res.end()
+    } else {
+        res.status(400).send("Sản phẩm không tồn tại!")
+    }
+})
+// Delete
+app.delete("/products/:id", (req, res) => {
+    const id = req.params.id
+    const productIndex = data.findIndex(item => item.id == id)
+    if (productIndex >= 0) {
+        data.splice(productIndex, 1)
+        res.json(data)
+        res.end()
+    } else {
+        res.status(400).send("Sản phẩm không tồn tại!")
+    }
+
 })
 // Routings
 // Get product by ID
