6f2fe52fc7109724507e914bfffdc1edfd44f3cf
tuva9 <tuva9@fpt.edu.vn>
2023-03-14 12:51:59 +0700

    Buổi 4: Refactor code

--- Lesson3/src/routers/product.js
+++ Lesson3/src/routers/product.js
@@ -1,5 +1,5 @@
-// const express = require('express')
-import express from "express"
+import express from "express";
+
 const router = express.Router()
 
 const data = [
@@ -8,9 +8,63 @@ const data = [
     {id: 3, name: "Màn hình", price: 500},
 ]
 
+// Miiddleware
+router.use((req, res, next) => {
+    console.log(req.url);
+    next()
+})
+
 router.get('/products', (req, res) => {
+    res.send(data)
+    res.end()
+})
+
+// Get product by ID
+router.get('/products/:id', (req, res) => {
+    const id = req.params.id
+    const product = data.find((item) => item.id == id)
+    if (product) {
+        res.send(product)
+    } else {
+        res.status(400).send("San pham khong ton tai!")
+    }
+})
+
+// Created
+router.post('/products', (req, res) => {
+    // console.log(req, "create product");
+    const newData = req.body
+    data.push(newData)
     res.send(data)
 })
 
-// module.exports = router
+// Updated
+router.put('/products/:id', (req, res) => {
+    const id = req.params.id
+    const updateData = req.body
+    const productIndex = data.findIndex(item => item.id == id)
+    if (productIndex >= 0) {
+        data[productIndex] = {...data[productIndex], ...updateData}
+        console.log(updateData);
+        res.send(data[productIndex])
+        res.end()
+    } else {
+        res.status(400).send("Sản phẩm không tồn tại!")
+    }
+})
+
+// Delete
+router.delete("/products/:id", (req, res) => {
+    const id = req.params.id
+    const productIndex = data.findIndex(item => item.id == id)
+    if (productIndex >= 0) {
+        data.splice(productIndex, 1)
+        res.json(data)
+        res.end()
+    } else {
+        res.status(400).send("Sản phẩm không tồn tại!")
+    }
+
+})
+
 export default router
--- Lesson3/src/server.js
+++ Lesson3/src/server.js
@@ -1,17 +1,30 @@
-// const express = require('express')
 import express from 'express'
-// const productRouter = require('./routers/product')
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
 import productRouter from './routers/product.js'
 
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
 const app = express()
+// Body-parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send("<h1>Hello world</h1>")
-    res.end()
-})
 // Router
 app.use('/api', productRouter)
+
+app.get('/', (req, res) => {
+    const html = fs.readFileSync(path.join(__dirname, "views/home.html"), "utf-8")
+    res.send(html)
+    res.end()
+})
+
 
 app.listen(8000, () => {
     console.log("Server running on port 8000");
