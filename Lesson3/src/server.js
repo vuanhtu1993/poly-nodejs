0358d61429a2014e9e24cbd2b61d740d498c4919
tuva9 <tuva9@fpt.edu.vn>
2023-03-14 13:48:38 +0700

    Buổi 4: Upload image

--- Lesson3/src/server.js
+++ Lesson3/src/server.js
@@ -4,20 +4,25 @@ import path from 'path'
 import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser';
 import productRouter from './routers/product.js'
+import fileRouter from './routers/image.js'
 
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
 
 const app = express()
-// Body-parser Middleware
+// Middleware
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 
+// Static file
+app.use(express.static("src/public"))
+
 // Router
 app.use('/api', productRouter)
+app.use('/upload', fileRouter)
 
 app.get('/', (req, res) => {
     const html = fs.readFileSync(path.join(__dirname, "views/home.html"), "utf-8")
