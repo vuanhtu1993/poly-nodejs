57a7c4b7c41a3ea9184ded6b98d5d29e34f47b99
tuva9 <tuva9@fpt.edu.vn>
2023-03-16 12:46:44 +0700

    Buổi 5: Refactor

--- Lesson3/src/routers/product.js
+++ Lesson3/src/routers/product.js
@@ -3,9 +3,9 @@ import express from "express";
 const router = express.Router()
 
 const data = [
-    {id: 1, name: "Chuột", price: 100},
-    {id: 2, name: "Bàn phím", price: 200},
-    {id: 3, name: "Màn hình", price: 500},
+    { id: 1, name: "Chuột", price: 100 },
+    { id: 2, name: "Bàn phím", price: 200 },
+    { id: 3, name: "Màn hình", price: 500 },
 ]
 
 // Miiddleware
@@ -15,18 +15,34 @@ router.use((req, res, next) => {
 })
 
 router.get('/products', (req, res) => {
-    res.send(data)
-    res.end()
+    try {
+        res.send(data)
+        res.end()
+    } catch (error) {
+        res.status(500).send({
+            message: "Lỗi server"
+        })
+        res.end()
+    }
 })
 
 // Get product by ID
 router.get('/products/:id', (req, res) => {
-    const id = req.params.id
-    const product = data.find((item) => item.id == id)
-    if (product) {
-        res.send(product)
-    } else {
-        res.status(400).send("San pham khong ton tai!")
+    try {
+        const id = req.params.id
+        const product = data.find((item) => item.id == id)
+        if (product) {
+            res.send(product)
+        } else {
+            res.status(400).send({
+                message: "San pham khong ton tai!"
+            })
+        }
+    } catch(error) {
+        res.status(500).send({
+            message: "Lỗi server"
+        })
+        res.end()
     }
 })
 
@@ -35,7 +51,9 @@ router.post('/products', (req, res) => {
     // console.log(req, "create product");
     const newData = req.body
     data.push(newData)
-    res.send(data)
+    res.send({
+        message: "Thêm mới thành công"
+    })
 })
 
 // Updated
@@ -44,7 +62,7 @@ router.put('/products/:id', (req, res) => {
     const updateData = req.body
     const productIndex = data.findIndex(item => item.id == id)
     if (productIndex >= 0) {
-        data[productIndex] = {...data[productIndex], ...updateData}
+        data[productIndex] = { ...data[productIndex], ...updateData }
         console.log(updateData);
         res.send(data[productIndex])
         res.end()
@@ -55,14 +73,21 @@ router.put('/products/:id', (req, res) => {
 
 // Delete
 router.delete("/products/:id", (req, res) => {
-    const id = req.params.id
-    const productIndex = data.findIndex(item => item.id == id)
-    if (productIndex >= 0) {
-        data.splice(productIndex, 1)
-        res.json(data)
+    try {
+        const id = req.params.id
+        const productIndex = data.findIndex(item => item.id == id)
+        if (productIndex >= 0) {
+            data.splice(productIndex, 1)
+            res.json(data)
+            res.end()
+        } else {
+            res.status(400).send("Sản phẩm không tồn tại!")
+        }
+    } catch(error) {
+        res.status(500).send({
+            message: "Lỗi server"
+        })
         res.end()
-    } else {
-        res.status(400).send("Sản phẩm không tồn tại!")
     }
 
 })
