966107fae4c7391b1148e94aca9834c992787191
tuva9 <tuva9@fpt.edu.vn>
2023-03-30 13:36:56 +0700

    Buổi 11: Mã hoá password + Signin

--- Lesson6/src/controllers/user.js
+++ Lesson6/src/controllers/user.js
@@ -1,5 +1,8 @@
 import User from "../models/user";
 import Joi from 'joi'
+import bcrypt from 'bcrypt'
+
+const salt = bcrypt.genSaltSync(10);
 
 const userSchema = Joi.object({
     firstname: Joi.string().required(),
@@ -23,13 +26,53 @@ export const signup = async (req, res) => {
                 message: error.details[0].message
             })
         } else {
-            const data = await User.create(body)
+            const hash = bcrypt.hashSync(body.password, salt);
+            const data = await User.create({...body, password: hash})
             res.send({
                 message: "Đăng kí thành công",
                 data: data
             })
         }
     } catch(err) {
+        res.status(500).send({
+            message: err
+        })
+    }
+}
+
+const userSigninSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required()
+})
+
+export const signin = async (req, res) => {
+    try {
+        const body = req.body
+        const {error} = userSigninSchema.validate(body)
+        if (error) {
+            res.status(400).send({
+                message: error.details[0].message
+            })
+        } else {
+            const user = await User.findOne({email: body.email})
+            console.log(user);
+            if(user) {
+                if(bcrypt.compareSync(body.password, user.password)) {
+                    res.send({
+                        message: "Đăng nhập thành công"
+                    }) 
+                } else {
+                    res.status(400).send({
+                        message: "Tên đăng nhập hoặc mật khẩu sai"
+                    })  
+                }
+            } else {
+                res.status(400).send({
+                    message: "Tên đăng nhập hoặc mật khẩu sai"
+                })  
+            }
+        }
+    } catch (err) {
         res.status(500).send({
             message: err
         })
--- Lesson6/src/routers/user.js
+++ Lesson6/src/routers/user.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { signup } from '../controllers/user'
+import { signin, signup } from '../controllers/user'
 
 const userRouter = express.Router()
 
 userRouter.post('/signup', signup)
+userRouter.post('/signin', signin)
 
 export default userRouter
